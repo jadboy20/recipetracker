2cae53ab8b29079685518d0dfc76941283820bf3
James D <jamesdixon117@gmail.com>
2021-08-08 15:13:30 +1000

    Add semantic ui to react frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { Header, Button } from 'semantic-ui-react'
+
 
 function App() {
+    fetch('http://192.168.0.83/backend/ingredients/all', {
+        method: 'GET',
+        mode: 'cors'
+    })
+    .then(response => console.log(response.text()));
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='padding'></div>
+        <Header as='h1'>Recipe Summary</Header>
+        <logo></logo>
+
     </div>
   );
 }
